eae7d0dc1af690d578f048e473a8bfd1322cd36e
Anastasia Stefanescu <anastasia@stefanescu.eu>
2025-03-16 16:25:46 +0200

    small merge fix, works fine

--- src/main/java/ro/unibuc/careerquest/controller/CVController.java
+++ src/main/java/ro/unibuc/careerquest/controller/CVController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import ro.unibuc.careerquest.data.CVCompCreation;
+import ro.unibuc.careerquest.dto.CVCompCreation;
 import ro.unibuc.careerquest.data.CVComponent;
 import ro.unibuc.careerquest.dto.CV;
 import ro.unibuc.careerquest.dto.CVCreation;
--- src/main/java/ro/unibuc/hello/data/InformationRepository.java
+++ src/main/java/ro/unibuc/hello/data/InformationRepository.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import ro.unibuc.hello.data.InformationEntity;
 
 /**
  * No need to implement this interface.
