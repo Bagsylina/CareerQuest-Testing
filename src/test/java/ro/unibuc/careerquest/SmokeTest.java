6669274e7efbf9a6d1ca096b4c24c2c4ef4935c6
Bagsylina <fabian.anghel.2012@gmail.com>
2025-04-28 07:48:20 +0000

    added smoke test

---
+++ src/test/java/ro/unibuc/careerquest/SmokeTest.java
@@ -0,0 +1,13 @@
+package ro.unibuc.careerquest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmokeTest {
+
+    @Test
+    void contextLoads() {
+        // This test will fail if the application context cannot start
+    }
+}
