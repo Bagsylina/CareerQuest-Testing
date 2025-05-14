36d69f30d39e330ea7fd0dddc81a96e67467f3c2
Bagsylina <fabian.anghel.2012@gmail.com>
2025-05-14 08:46:15 +0000

    separated smoke test from build

--- src/test/java/ro/unibuc/careerquest/SmokeTest.java
+++ src/test/java/ro/unibuc/careerquest/SmokeTest.java
@@ -1,9 +1,11 @@
 package ro.unibuc.careerquest;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Tag("SmokeTest")
 class SmokeTest {
 
     @Test
