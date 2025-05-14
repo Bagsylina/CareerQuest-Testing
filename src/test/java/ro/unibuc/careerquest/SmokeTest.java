1a57fbf5e0b1a735ed3b635e8edc1e681ebdb8a4
Bagsylina <fabian.anghel.2012@gmail.com>
2025-05-14 15:35:41 +0000

    added mongo container

--- src/test/java/ro/unibuc/careerquest/SmokeTest.java
+++ src/test/java/ro/unibuc/careerquest/SmokeTest.java
@@ -1,14 +1,17 @@
 package ro.unibuc.careerquest;
 
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertThat;
 
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
 
-import io.cucumber.java.lu.a.as;
 import ro.unibuc.careerquest.controller.ApplicationController;
 import ro.unibuc.careerquest.controller.CVController;
 import ro.unibuc.careerquest.controller.EmployerController;
@@ -37,6 +40,29 @@ class SmokeTest {
 
     @Autowired
     private UserController userController;
+
+    @Container
+    public static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:6.0.20")
+            .withExposedPorts(27017)
+            .withSharding();
+    
+    @BeforeAll
+    public static void setUp() {
+        mongoDBContainer.start();
+    }
+        
+    @AfterAll
+    public static void tearDown() {
+        mongoDBContainer.stop();
+    }
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry registry) {
+        final String MONGO_URL = "mongodb://host.docker.internal:";
+        final String PORT = String.valueOf(mongoDBContainer.getMappedPort(27017));
+
+        registry.add("mongodb.connection.url", () -> MONGO_URL + PORT);
+    }
 
     @Test
     void contextLoads() {
